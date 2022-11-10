fdaa44f2f33adfd9c380d850f51bee9ac8aebef6
magmesser <magmesser@gmail.com>
2022-11-10 17:23:40 -0500

    added search history and readme

---
+++ Assets/js/script.js
@@ -0,0 +1,143 @@
+var APIKey = "548fe189ee5f7583e55d370db86c3c22";
+var currentDay = moment().format("L");
+var forecastContainer = document.querySelector("#forecastContainer");
+var fiveDayContainer = document.querySelector("#fiveDayContainer");
+var searchForm = document.querySelector("#searchForm");
+var cityInput = document.querySelector("#cityInput");
+var searchHistory = document.getElementById("previous-search");
+var weather = document.getElementById("weather");
+var temperature = document.getElementById("temperature");
+var humidity = document.getElementById("humidity");
+var wind = document.getElementById("wind");
+
+// api call per city input
+var getWeatherForecast = function (city) {
+  var apiURL =
+    "http://api.openweathermap.org/data/2.5/weather?q=" +
+    city +
+    "&appid=" +
+    APIKey +
+    "&units=imperial";
+
+  fetch(apiURL)
+    .then(function (response) {
+      if (response.ok) {
+        response.json().then(function (data) {
+          displayWeatherSearch(data);
+        });
+      } else {
+        alert("Error: " + response.statusText);
+      }
+    })
+
+    .catch(function (error) {
+      alert("Unable to reach OpenWeather");
+    });
+};
+
+// user input submit
+var citySearchSubmit = function (event) {
+  event.preventDefault();
+
+  var cityName = cityInput.value.trim();
+
+  if (cityName) {
+    getWeatherForecast(cityName);
+
+    cityInput.value = "";
+  } else {
+    alert("Please enter a city");
+  }
+};
+
+// manipulating called data
+var displayWeatherSearch = function (weatherData) {
+  var weatherHeadingEl = document.createElement("h2");
+  weatherHeadingEl.textContent = weatherData.name + " (" + currentDay + ")";
+  forecastContainer.append(weatherHeadingEl);
+
+  weather.textContent = "Weather: " + weatherData.weather[0].main;
+  temperature.textContent = "Temperature: " + weatherData.main.temp + "°F";
+  humidity.textContent = "Humidity: " + weatherData.main.humidity + "%";
+  wind.textContent = "Wind: " + weatherData.wind.speed + " mph";
+
+  var history = [];
+  var value = weatherData.name;
+  var historyBtn = document.createElement("button");
+  historyBtn.textContent = value;
+  searchHistory.append(historyBtn);
+
+  history.push(value);
+
+  console.log(value);
+  localStorage.setItem("history", value);
+  localStorage.getItem("history", value);
+
+  let lat = weatherData.coord.lat;
+  let lon = weatherData.coord.lon;
+
+  var apiCoordURL =
+    "http://api.openweathermap.org/data/2.5/forecast?lat=" +
+    lat +
+    "&lon=" +
+    lon +
+    "&appid=" +
+    APIKey +
+    "&units=imperial";
+
+  // api call for lat and lon
+  fetch(apiCoordURL).then(function (response) {
+    response.json().then(function (data) {
+      console.log(data);
+
+      for (var i = 1; i <= 5; i++) {
+        // card
+        var forecastCardEl = document.createElement("div");
+        forecastCardEl.classList =
+          "col-2 py-3 p-1 card text-center text-white bg-info";
+
+        // card body
+        var cardBodyEl = document.createElement("div");
+        cardBodyEl.classList = "card-body p-1";
+        forecastCardEl.append(cardBodyEl);
+
+        // card title
+        var cardTitleEl = document.createElement("h5");
+        cardTitleEl.classList = "card-title";
+        cardTitleEl.textContent = moment().add(i, "d").format("l");
+        cardBodyEl.append(cardTitleEl);
+
+        // card UL
+        var cardListEl = document.createElement("ul");
+        cardListEl.classList = "card-text";
+        cardBodyEl.append(cardListEl);
+
+        // card temp
+        var cardTempEl = document.createElement("li");
+        cardTempEl.textContent = "Temp: " + data.list[i].main.temp + "°F";
+        cardListEl.append(cardTempEl);
+
+        // // card weather
+        // var cardWeatherEl = document.createElement('li');
+        // cardWeatherEl.textContent = "Weather: " + data.list[i].weather[0].main;
+        // cardListEl.append(cardWeatherEl);
+
+        // card humidity
+        var cardHumidEl = document.createElement("li");
+        cardHumidEl.textContent =
+          "Humidity: " + data.list[i].main.humidity + "%";
+        cardListEl.append(cardHumidEl);
+
+        // card wind
+        var cardWindEl = document.createElement("li");
+        cardWindEl.textContent = "Wind: " + data.list[i].wind.speed + " mph";
+        cardListEl.append(cardWindEl);
+
+        fiveDayContainer.append(forecastCardEl);
+      }
+    });
+  });
+};
+
+// submit event listener
+searchForm.addEventListener("submit", citySearchSubmit);
--- README.md
+++ README.md
@@ -1,1 +1,34 @@
-test
+# Weather Dashboard API Project
+
+## User Story 
+```md
+AS A traveler
+I WANT to see the weather outlook for multiple cities
+SO THAT I can plan a trip accordingly
+```
+
+## Description
+
+This project tested my knowledge of third-party and server-side APIs, JavaScript, jQuery.  I ensured that the code was debugged to the best of my ability and ran correctly.  This assignment was challenging but the implementation of server-side APIs has helped to further understand JS concepts. When I got stuck I used Google to research local storage and read through StackOverflow, and watched YouTube videos, as well as researching specific elements in W3Schools and MDN. 
+
+---
+
+## Links
+
+- Click the link below to access the deployed site on GitHub Pages.<br>
+https://magmesser.github.io/weather-dashboard/
+
+
+- Click the link below to access to application repository on GitHub. <br>
+https://github.com/magmesser/weather-dashboard
+
+
+---
+
+## Screenshot
+
+The following screenshot shows the web application's appearance: 
+
+
+![Screenshot of the application](./Assets/images/Screenshot.png)
+
